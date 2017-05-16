9030a8e614ef68650ec4074d1a61283ff152a043
Stanya PALMARO <spalmaro@e2r5p3.42.fr>
2017-05-16 15:36:55 +0200

    canvas appendchild

--- js/tag_the_web.js
+++ js/tag_the_web.js
@@ -2,6 +2,13 @@ var	w = window.innerWidth;
 var	h = window.innerHeight;
 var wild = false;
 var CheminComplet = document.location.href;
+
+var canvas = document.createElement('canvas');
+canvas.id = 'drawing_canvas';
+canvas.style.width = w;
+canvas.style.height = h;
+document.appendChild(canvas);
+
 /*
 function deactivate_sm(){
 	wild = true;
