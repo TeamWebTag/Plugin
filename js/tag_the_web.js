569ee1407781ad55fa265ffb50a669c515cdf1d9
Elise <Elise@MacBook-Pro-Elise.local>
2017-05-21 17:13:15 +0200

    color yaaaay

--- js/tag_the_web.js
+++ js/tag_the_web.js
@@ -23,6 +23,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		tools.style.opacity = '1';
 		tools.style.position = 'absolute';
 		tools.style.backgroundColor = 'beige';
+		tools.style.boxShadow = 'rgba(0, 0, 0, 0.3) 0px 6px 10px';
 		tools.style.top = '0px';
 		tools.style.left = '0px';
 		document.body.appendChild(tools);
@@ -33,6 +34,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		crayon.style.height = '50px';
 		crayon.style.top =  '15px';
 		crayon.style.left = '15px';
+		crayon.style.cursor = 'pointer';
 		crayon.style.position = 'absolute';
 		document.getElementById('using_tools').appendChild(crayon);
 		var eraser = document.createElement("img"); 
@@ -42,26 +44,40 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		eraser.style.height = '50px';
 		eraser.style.top =  '80px';
 		eraser.style.left = '15px';
+		eraser.style.cursor = 'pointer';
 		eraser.style.position = 'absolute';
 		document.getElementById('using_tools').appendChild(eraser);
+		var libcolor = document.createElement("script");
+		libcolor.src = chrome.runtime.getURL('jscolor/jscolor.js');
+		document.body.appendChild(libcolor);
+		var butcolor = document.createElement("button");
+		butcolor.className = "jscolor {valueElement:null,value:'ffffff'}";
+    	butcolor.style.border = "2px solid black";
+    	butcolor.style.width = '50px';
+		butcolor.style.height = '50px';
+		butcolor.style.top =  '145px';
+		butcolor.style.left = '15px';
+		butcolor.style.cursor = 'pointer';
+		butcolor.style.position = 'absolute';
+		document.getElementById('using_tools').appendChild(butcolor);
 	} else if (request.todo == "desactivateExt"){
 		var oldcanvas = document.getElementById('drawing_canvas');
-		if (oldcanvas !== undefined)
+		if (oldcanvas !== null)
 		{
 			document.body.removeChild(oldcanvas);
 			window.location.reload();
 		}
-		tools.display = '';
+		// tools.display = '';
 	}
 	if (request.todo == "actTools"){
 		var canTool = document.getElementById('drawing_canvas');
 		canTool.style.pointerEvents = 'auto';
-		tools.display = "";
+		// tools.display = "";
 		// var pen = tools.createElement("div");
 	}
 	else if (request.todo == "desTools"){
 		var canTool = document.getElementById('drawing_canvas');
 		canTool.style.pointerEvents = 'none';
-		tools.display = '';
+		// tools.display = '';
 	}
 });
