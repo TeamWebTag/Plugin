7f40496293fc86084d6b8aa6987c92e713aa6b7e
Elise <Elise@MacBook-Pro-Elise.local>
2017-05-21 17:55:57 +0200

    il manque les outils

--- js/tag_the_web.js
+++ js/tag_the_web.js
@@ -26,6 +26,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		tools.style.boxShadow = 'rgba(0, 0, 0, 0.3) 0px 6px 10px';
 		tools.style.top = '0px';
 		tools.style.left = '0px';
+		tools.style.opacity = '0';
 		document.body.appendChild(tools);
 		var crayon = document.createElement("img"); 
 		crayon.id = 'pencil';
@@ -34,6 +35,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		crayon.style.height = '50px';
 		crayon.style.top =  '15px';
 		crayon.style.left = '15px';
+		crayon.style.borderRadius = '8px 8px 8px 8px';
 		crayon.style.cursor = 'pointer';
 		crayon.style.position = 'absolute';
 		document.getElementById('using_tools').appendChild(crayon);
@@ -44,6 +46,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		eraser.style.height = '50px';
 		eraser.style.top =  '80px';
 		eraser.style.left = '15px';
+		eraser.style.borderRadius = '8px 8px 8px 8px';
 		eraser.style.cursor = 'pointer';
 		eraser.style.position = 'absolute';
 		document.getElementById('using_tools').appendChild(eraser);
@@ -57,6 +60,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 		butcolor.style.height = '50px';
 		butcolor.style.top =  '145px';
 		butcolor.style.left = '15px';
+		butcolor.style.borderRadius = '8px 8px 8px 8px';
 		butcolor.style.cursor = 'pointer';
 		butcolor.style.position = 'absolute';
 		document.getElementById('using_tools').appendChild(butcolor);
@@ -67,17 +71,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
 			document.body.removeChild(oldcanvas);
 			window.location.reload();
 		}
-		// tools.display = '';
 	}
 	if (request.todo == "actTools"){
 		var canTool = document.getElementById('drawing_canvas');
-		canTool.style.pointerEvents = 'auto';
-		// tools.display = "";
-		// var pen = tools.createElement("div");
-	}
-	else if (request.todo == "desTools"){
+		// canTool.style.pointerEvents = 'auto';
+		document.getElementById('using_tools').style.opacity = '1';
+	} else if (request.todo == "desTools"){
 		var canTool = document.getElementById('drawing_canvas');
 		canTool.style.pointerEvents = 'none';
-		// tools.display = '';
+		document.getElementById('using_tools').style.opacity = '0';
 	}
 });
